76a3ceb24b9573346ee0877cf2b468b9742fcc7c
Seth McLaughlin <Seth.d.mclaughlin@gmail.com>
2020-04-15 09:20:35 -0400

    Add project files.

---
+++ RestaurantRaterAPI/Models/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantRaterAPI.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+    }
+}
